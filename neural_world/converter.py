fad1f4ff8b53b0f31c4e55e6ee4ea9f2307ce21b
Aluriak <lucas.bourneuf@laposte.net>
2015-11-08 23:51:39 +0100

    use pygraphviz instead of graphviz + change working API

--- neural_world/converter.py
+++ neural_world/converter.py
@@ -4,7 +4,7 @@ graph into a standard graph format.
 
 """
 import tempfile
-from graphviz import Digraph
+import pygraphviz as pgv
 from collections import defaultdict
 
 from neural_world.atoms import split as atoms_split
@@ -13,30 +13,43 @@ from neural_world.commons import NeuronType
 
 def graphdict_to_dot(graph_dict):
     """Return the DOT equivalent to the graph describes in given dict"""
-    graph = Digraph()
+    graph = pgv.AGraph(strict=False, directed=True)
     for pred, nexts in graph_dict.items():
-        pred = str(pred)
-        graph.node(pred)
-        for succ in (str(_) for _ in nexts):
-            graph.node(succ)
-            graph.edge(pred, succ)
-    return graph.source
+        # pred = str(pred)
+        graph.add_node(pred)
+        # for succ in (str(_) for _ in nexts):
+        for succ in (_ for _ in nexts):
+            graph.add_node(succ)
+            graph.add_edge(pred, succ)
+    return graph.string()
+
 
 def network_atoms_to_dot(network_atoms):
     """Return the DOT formatted string equivalent of given neural network"""
-    return network_atoms_to_graphviz(network_atoms).source
+    return str(network_atoms_to_graphviz(network_atoms))
 
 
-def network_atoms_rendering(network_atoms, filename=None, directory=None):
-    """Print the given network_atoms in pdf. Spam your working screen."""
-    return network_atoms_to_graphviz(network_atoms).render(
-        view=False, cleanup=True,  # don't show it, remove source
-        filename=filename, directory=directory
-    )
+def graph_rendering(graph_data, filename='output.png'):
+    """Draw the given graph in given filename.
+
+    graph_data: DOT formatted string describing a graph
+        (return value of  network_atoms_to_dot(1)),
+        or a pygraphviz.AGraph instance.
+    filename: file where the rendering will be saved.
+    return: the pygraphviz.AGraph instance that have been rendered.
+
+    """
+    # convert network_atoms in pgv.AGraph, if necessary
+    if isinstance(graph_data, pgv.AGraph):
+        graph = graph_data
+    else:
+        graph = pgv.AGraph(graph_data)  # create an AGraph directly from dot
+    graph.draw(filename, prog='dot')
+    return graph
 
 
 def network_atoms_to_graphviz(network_atoms):
-    """Create a graphviz.Digraph object from the given network_atoms."""
+    """Create a pygraphviz.AGraph object from the given network_atoms."""
     atoms = (atoms_split(a) for a in network_atoms.split('.') if a != '')
     graph = defaultdict(str)  # ID: type
     edges = []  # list of 2-tuple
@@ -59,7 +72,7 @@ def network_atoms_to_graphviz(network_atoms):
             LOGGER.error('UNHANDLED ATOMS: ' + str((name, args)) + '.')
 
     # Create and return the dot from graph and edges
-    dot = Digraph()
-    [dot.node(idn, str(idn) + '\n' + label) for idn, label in graph.items()]
-    [dot.edge(a, b) for a, b in edges]
+    dot = pgv.AGraph(strict=False, directed=True)
+    [dot.add_node(idn, label=str(idn) + '\n' + label) for idn, label in graph.items()]
+    [dot.add_edge(a, b) for a, b in edges]
     return dot
