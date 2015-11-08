b5df964b71d174583ad6f92d69a200713d3ac293
Aluriak <lucas.bourneuf@laposte.net>
2015-11-09 00:49:09 +0100

    use a graphiz layout + save final genealogy into dot and png

--- neural_world/observer/treebuilder.py
+++ neural_world/observer/treebuilder.py
@@ -11,6 +11,7 @@ from . import observer
 
 class TreeBuilder(observer.Observer):
     """Observer of World that maintain a tree of life"""
+    GRAPHVIZ_LAYOUT = converter.GraphvizLayout.twopi
 
     def __init__(self, archive_directory):
         self.tree = defaultdict(list)
@@ -23,6 +24,10 @@ class TreeBuilder(observer.Observer):
                 self.tree[parent.ID].append(new.ID)
 
     def postprocessing(self, world):
-        """Save the tree in file"""
-        with open(os.path.join(self.archive_directory, 'tree'), 'w') as fd:
-            fd.write(converter.graphdict_to_dot(self.tree))
+        """Save the tree in file, and its rendering"""
+        filename = os.path.join(self.archive_directory, 'tree')
+        dot = converter.graphdict_to_dot(self.tree)
+        with open(filename, 'w') as fd:
+            fd.write(dot)
+        converter.graph_rendering(dot, filename + '.png',
+                                  layout=TreeBuilder.GRAPHVIZ_LAYOUT)
