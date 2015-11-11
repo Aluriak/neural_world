7304f5e229130ba03a13b946912ae0630fe8cb57
Aluriak <lucas.bourneuf@laposte.net>
2015-11-11 01:12:22 +0100

    Automatic import of all classes in the observer directory

--- neural_world/observer/__init__.py
+++ neural_world/observer/__init__.py
@@ -3,7 +3,12 @@ This submodule contains all Observer implementations, as the Observer,
 Observable and signal classes/enum.
 
 """
+from os.path import dirname, relpath
+
+from neural_world.commons import import_classes
 from neural_world.observer.observer  import Signal, Observable, Observer
-from neural_world.observer.archivist import Archivist
-from neural_world.observer.worldview import TerminalWorldView
-from neural_world.observer.treebuilder import TreeBuilder
+
+
+# import all classes for all modules of observer submodule
+for cls in import_classes(relpath(dirname(__file__))):
+    globals()[cls.__name__] = cls
