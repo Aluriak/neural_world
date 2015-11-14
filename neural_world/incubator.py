71106a51c51ff3fe490a9e117470270600f536b4
Aluriak <lucas.bourneuf@laposte.net>
2015-11-14 17:43:13 +0100

    Incubator is now Configurable, and use the configuration

--- neural_world/incubator.py
+++ neural_world/incubator.py
@@ -7,29 +7,35 @@ from random import randint
 from functools import partial
 
 import neural_world.default as default
-from neural_world.commons    import NeuronType
-from neural_world.individual import Individual
+from neural_world.commons      import NeuronType
+from neural_world.individual   import Individual
+from neural_world.configurable import Configurable
 
 
-class Incubator:
+class Incubator(Configurable):
     """Spawn Individual instances"""
 
-    def __init__(self, mutator=None):
-        self.mutator = mutator
+    def __init__(self, config):
+        super().__init__(config=config, config_fields=[
+            'neuron_inter_mincount', 'neuron_inter_maxcount',
+            'neuron_edges_mincount', 'neuron_edges_maxcount',
+            'mutator',
+        ])
 
     def spawn(self):
-        nb_neuron = randint(2, 20)
-        total_nb_neuron = sum((
-            nb_neuron,
-            config.INPUT_NEURON_COUNT,
-            config.OUTPUT_NEURON_COUNT
-        ))
+        """Spawn Individual instances"""
+        # get data & shortcuts
+        nb_neuron = randint(self.neuron_inter_mincount,
+                            self.neuron_inter_maxcount)
+        total_nb_neuron = Individual.neuron_total_count(nb_neuron)
+        random.neuron_id = partial(randint, 1, total_nb_neuron + 1)
+        # types and edges random generation
         types = (random.choice(tuple(NeuronType.xano()))
                  for _ in range(nb_neuron))
-        random.neuron_id = partial(randint, 1, total_nb_neuron + 1)
         edges = (
             (random.neuron_id(), random.neuron_id())
-            for _ in range(randint(nb_neuron, nb_neuron * 5))
+            for _ in range(randint(self.neuron_edges_mincount,
+                                   self.neuron_edges_maxcount))
         )
 
         return Individual(
