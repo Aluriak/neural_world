3b6c0ac15cc6c6ff69834032e7757b7bff3b07ba
Aluriak <lucas.bourneuf@laposte.net>
2015-11-14 17:45:15 +0100

    Engine: perform the World's next step algorithm + can be generated from a Configuration

--- neural_world/engine.py
+++ neural_world/engine.py
@@ -2,6 +2,13 @@
 Invoker of actions, from View onto World.
 
 """
+import neural_world.commons as commons
+import neural_world.actions as action
+from neural_world.world import World
+from neural_world.commons import coroutine
+
+
+LOGGER = commons.logger()
 
 
 class Engine:
@@ -11,10 +18,30 @@ class Engine:
         self.commands = []
 
     def add(self, command):
-        self.commands.append(command)
+        try:
+            [self.commands.append(c) for c in command]
+        except TypeError:
+            self.commands.append(command)
 
     def invoke_all(self):
         [c.execute(self.world) for c in self.commands]
         self.commands = []
+
+    def apply(self, config):
+        """Apply given config then wait for the next."""
+        if not config.terminated:
+            # prepare the next amount of actions
+            for coords, obj in self.world:
+                obj.update(self, self.world.neighbors(coords), coords)
+            self.add(action.RegenerateNutrientsAction())
+            self.add(action.StepComputedAction())
+            # invoke them
+            self.invoke_all()
+
+    @staticmethod
+    def generate_from(config):
+        """Generate world according to config."""
+        world = World(config)
+        return Engine(world)
 
 
