e37322542f9716e0e60f23bd18a44219d2d98b44
Aluriak <lucas.bourneuf@laposte.net>
2016-03-27 15:24:20 +0200

    tests: __init__ defines mother class of ASP solving related TestCase classes

---
+++ neural_world/tests/__init__.py
@@ -0,0 +1,61 @@
+import unittest
+from pyasp.asp import TermSet
+
+from neural_world.neural_network import model_from, FILE_ASP_RUNNING
+
+
+def comparable_atoms(atoms:str) -> set:
+    """Return the same atoms, as comparable data"""
+    # make atoms iterable over each atom
+    # if atoms is a TermSet or a tuple, it is already done
+    if isinstance(atoms, str):
+        if ' ' in atoms:
+            atoms = atoms.split(' ')
+        else:
+            atoms = atoms.split('.')
+    # return atoms as a set
+    return frozenset(atom.strip('.  \n\t') for atom in atoms if len(atom))
+
+
+class NeuralNetworkTester(unittest.TestCase):
+    """Mother class of all ASP solving related unittest.TestCase classes.
+
+    Defines the assert_solving function, designed as high level ASP output test.
+
+    """
+
+    def __assert_solving(self, atoms, expected_result, solve_func):
+        solved = comparable_atoms(solve_func(atoms))
+        expected = comparable_atoms(expected_result)
+        # debug printings
+        # print('\n\tATOMS   : ' + atoms,
+              # '\n\tFOUND   : ' + solved,
+              # '\n\tEXPECTED: ' + expected,
+              # '\n\tEQUALITY: ' + str(expected == solved))
+        self.assertEqual(solved, expected)
+
+    def assert_cleaning(self, atoms, expected_result):
+        """Compare given atoms after cleaning and expected_result,
+        using self.assertEqual.
+
+        atoms -- TermSet or iterable of string or string of atoms sep. by space.
+        expected_result -- string of atoms separated by space.
+
+        """
+        return self.__assert_solving(atoms, expected_result, self._clean)
+
+    def assert_solving(self, atoms, expected_result):
+        """Compare given atoms after solving and expected_result,
+        using self.assertEqual.
+
+        atoms -- TermSet or iterable of string or string of atoms sep. by space.
+        expected_result -- string of atoms separated by space.
+
+        """
+        return self.__assert_solving(atoms, expected_result, self._run)
+
+    def _clean(self, atoms):
+        return model_from(atoms, FILE_ASP_CLEANING)
+
+    def _run(self, atoms):
+        return model_from(atoms, FILE_ASP_RUNNING)
