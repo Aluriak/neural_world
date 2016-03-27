f00bad197aa6093e074d646b4635c12fcceac633
Aluriak <lucas.bourneuf@laposte.net>
2016-03-27 18:58:16 +0200

    incubator: use the new Individual and NeuralNetwork API

--- neural_world/incubator.py
+++ neural_world/incubator.py
@@ -10,6 +10,7 @@ import neural_world.default as default
 from neural_world.commons    import NeuronType
 from neural_world.individual import Individual
 from neural_world.commons    import Configurable
+from neural_world.neural_network import NeuralNetwork
 
 
 class Incubator(Configurable):
@@ -21,27 +22,37 @@ class Incubator(Configurable):
             'neuron_edges_mincount', 'neuron_edges_maxcount',
             'mutator',
         ])
+        self.neuron_types = NeuronType.ixano()
 
     def spawn(self):
         """Spawn Individual instances"""
         # get data & shortcuts
-        nb_neuron = randint(self.neuron_inter_mincount,
-                            self.neuron_inter_maxcount)
-        total_nb_neuron = Individual.neuron_total_count(nb_neuron)
-        random.neuron_id = partial(randint, 1, total_nb_neuron + 1)
-        # types and edges random generation
-        types = (random.choice(tuple(NeuronType.xano()))
-                 for _ in range(nb_neuron + 4))
+        nb_inter_neuron = randint(self.neuron_inter_mincount,
+                                  self.neuron_inter_maxcount)
+        nb_neuron = self.neuron_total_count(nb_inter_neuron)
+        random.neuron_id = partial(randint, 1, nb_neuron)
+        # neuron types and edges random generation
+        neuron_types = (
+            random.choice(self.neuron_types)
+            for _ in range(nb_neuron - default.INPUT_NEURON_COUNT)
+        )
         edges = (
             (random.neuron_id(), random.neuron_id())
             for _ in range(randint(self.neuron_edges_mincount,
                                    self.neuron_edges_maxcount))
         )
 
-        return Individual(
-            nb_intermediate_neuron=nb_neuron,
-            neuron_types=types,
+        # Construction of the neural network
+        neural_network = NeuralNetwork(
             edges=edges,
+            nb_inter_neuron=nb_inter_neuron,
+            nb_input_neuron=default.INPUT_NEURON_COUNT,
+            nb_output_neuron=default.OUTPUT_NEURON_COUNT,
+            neuron_types=neuron_types,
+        )
+
+        return Individual(
+            neural_network=neural_network,
             energy=10,
         )
 
@@ -53,4 +64,13 @@ class Incubator(Configurable):
         to the new clone.
 
         """
-        return indiv.clonage(self.mutator, energy)
+        return indiv.clone(self.mutator, energy)
+
+
+    def neuron_total_count(self, nb_intermediate_neuron):
+        """Return the total number of neuron present in an individual
+        with given number of intermediate neuron."""
+        return sum((
+            default.INPUT_NEURON_COUNT, default.OUTPUT_NEURON_COUNT,
+            nb_intermediate_neuron
+        ))
