ac4e35d385ff3fe366c05d494b3b74d8b2fcd99f
Aluriak <lucas.bourneuf@laposte.net>
2016-03-27 15:25:40 +0200

    tests: test_neural_network use the module definition (see __init__.py)

--- neural_world/tests/test_neural_network.py
+++ neural_world/tests/test_neural_network.py
@@ -5,14 +5,15 @@ Unit tests for neural networks routines.
 import unittest
 
 from neural_world import neural_network
+from neural_world.tests import NeuralNetworkTester
 
 
-class TestFunctionsNeuralNetwork(unittest.TestCase):
+class TestFunctionsNeuralNetwork(NeuralNetworkTester):
 
     def setUp(self):
         pass
 
 
     def test_simple(self):
-        nn = 'neuron(1,i). neuron(2,i). neuron(3,o). edge(1,3). edge(1,2).'
-        # TODO
+        nn = 'neuron(1,i). neuron(2,i). neuron(3,o). output(3). edge(1,3). edge(1,2).'
+        self.assert_solving(nn, neural_network.clean(nn))
