b192924058aa7ad565a28f8ce54f7f3073ab7050
Aluriak <lucas.bourneuf@laposte.net>
2016-04-09 14:58:21 +0200

    neural_network: new API -- major rewritting
    Now, the neural network is decoupled from neurons roles: the
    NeuralNetworkEngine define just the solving of the neural network,
    and its subclass NeuralNetwork defines how define input neurons states,
    and how to react to output neurons states.
    
    This update seems to slows some treatments (especially network solving
    and neural network construction), but allow a simple and efficient way
    to design the neural network without having to hack the whole class each
    time.

--- neural_world/neural_network.py
+++ neural_world/neural_network.py
@@ -8,12 +8,14 @@ import itertools
 from functools   import partial
 from collections import deque
 
+from neural_world import actions
 from neural_world import default
 from neural_world import atoms
 from neural_world import commons
 from neural_world import solving
 from neural_world.commons import (FILE_ASP_RUNNING, FILE_ASP_CLEANING,
                                   NeuronType, Direction)
+from neural_world.neural_network_engine import NeuralNetworkEngine, MINIMAL_NEURON_ID
 
 
 LOGGER = commons.logger(commons.SUBLOGGER_LIFE)
@@ -36,22 +38,8 @@ def square_to_input_neurons(square):
     return contains_nutrients, contains_individuals
 
 
-class NeuralNetwork:
-    """Encapsulation of many routines about the simple neural network string.
-
-    Each NeuralNetwork instance figure a set of atoms, and provides an API for
-    building new NeuralNetwork, reacting to neighbors states and cloning.
-
-    A neural network is a string, containing ASP-formatted data as atoms as:
-        - neuron(I,T): neuron of id I is of type T (T in IXANO)
-        - output(I,O): neuron of id I is an output neuron to direction D
-        - memwrite(I,A): neuron of id I is a memory writing neuron at address A
-        - edge(I,J): there is an edge between neurons of id I and J
-
-    The output types are:
-        - the directions, defined by the Direction enumeration
-        - memory indexes, in [0;memory_size[
-
+class NeuralNetwork(NeuralNetworkEngine):
+    """Definition of the neural network behaviors.
 
     About the memory:
 
@@ -66,119 +54,65 @@ class NeuralNetwork:
     associated address is inverted (True <-> False).
 
     """
+    MINIMAL_NEURON_ID = MINIMAL_NEURON_ID
 
-
-    def __init__(self, nb_inter_neuron:int, edges:iter,
-                 neuron_types:iter, memory_size:int,
-                 nb_input_neuron:int=default.INPUT_NEURON_COUNT,
-                 nb_output_neuron:int=default.OUTPUT_NEURON_COUNT):
-        # Management of neural network data
-        self.nb_intermediate_neuron = nb_inter_neuron
-        self.nb_input_neuron = nb_input_neuron
-        self.nb_output_neuron = nb_output_neuron
-        self.edges = tuple(edges)
-        self.neuron_types = tuple(neuron_types)
+    def __init__(self, nb_inter_neuron:int, memory_size:int,
+                 nb_neighbor:int=default.NEIGHBOR_COUNT,
+                 nb_unit_type:int=default.UNIT_TYPE_COUNT, **kwargs):
+        # shortcuts
+        nb_neighbor_neuron = NeuralNetwork.nb_neighbor_neuron(nb_neighbor, nb_unit_type)
+        # fields and finally a call to base class init
         self.memory = [False] * memory_size
-        neuron_type = (_ for _ in self.neuron_types)  # generator
-        # generator of neuron id, exhausted when no remaining neurons
-        neuron_ids = (idx + 1 for idx in range(self.nb_neuron))
-        # Construction of the neural network
-        network_atoms = '.'.join(itertools.chain(
-            # input neurons
-            ('neuron(' + str(idn) + ','
-             + default.INPUT_NEURON_TYPE.value + ')'
-             for idn in itertools.islice(neuron_ids, 0, nb_input_neuron)),
-            # input memory neurons
-            ('neuron(' + str(idn) + ','
-             + default.INPUT_NEURON_TYPE.value + ')'
-             for idn in itertools.islice(neuron_ids, 0, memory_size)),
-            # intermediate neurons
-            ('neuron(' + str(idn) + ',' + next(neuron_type).value + ')'
-             for idn in itertools.islice(neuron_ids, 0, nb_inter_neuron)),
-            # output memory neurons: give their type and their output status.
-            ('neuron(' + str(idn) + ',' + next(neuron_type).value + ').'
-             + 'memwrite(' + str(idn) + ',' + str(mem_addr) + ')'
-             for mem_addr, idn in enumerate(itertools.islice(neuron_ids,
-                                                             0, memory_size))),
-            # output neurons: give their type and their output status.
-            ('neuron(' + str(idn) + ',' + next(neuron_type).value + ').'
-             + 'output(' + str(idn) + ',{})'  # this neuron is an output
-             for idn in neuron_ids),
-            # edges
-            ('edge(' + str(id1) + ',' + str(id2) + ')'
-             for id1, id2 in self.edges)
-        )).format(*Direction.names()) + '.'
-        assert network_atoms.count('neuron') == self.nb_neuron
-        assert network_atoms.count('edge') == len(self.edges)
-        assert network_atoms.count('memwrite') == memory_size
-        assert 'up' in network_atoms
-        assert 'right' in network_atoms
-        assert 'down' in network_atoms
-        assert 'left' in network_atoms
-        # Cleaning, for remove useless data
-        self.neural_network_all = network_atoms
-        self.neural_network = NeuralNetwork.cleaned(network_atoms)
-        if len(self.neural_network) > 0:
-            assert self.neural_network[-1] == '.'
-        LOGGER.debug('NEW NEURAL NETWORK: ' + self.neural_network_all)
-        LOGGER.debug('CLEANED: ' + self.neural_network)
+        super().__init__(nb_intermediate_neuron=nb_inter_neuron, **kwargs,
+                         inputs=((self.neighbors, nb_neighbor_neuron),
+                                 (self.read_memory, self.memory_size)),
+                         outputs=((self.directions, len(Direction)),
+                                  (self.write_memory, self.memory_size)))
 
 
-    @property
-    def nb_neuron(self):
-        """Return the total number of neuron"""
-        return NeuralNetwork.neuron_total_count(self.nb_intermediate_neuron,
-                                                self.memory_size,
-                                                self.nb_input_neuron,
-                                                self.nb_output_neuron)
+    def neighbors(self, kwargs):
+        return tuple(kwargs['neighbors'])
+
+
+    def directions(self, states, kwargs):
+        directions = tuple(d for s, d in zip(states, Direction) if s)
+        yield actions.MoveAction(kwargs['individual'], kwargs['coords'], directions)
+
+
+    def read_memory(self, kwargs):
+        return self.memory
+
+
+    def write_memory(self, states, kwargs):
+        """Change the memory, according to given states"""
+        assert len(states) == len(self.memory)
+        # if a state is True, the associated value in memory is changed:
+        # Memory values:    0 1 0 1
+        # States values:    0 0 1 1    < this is XOR !
+        # New memory   :    0 1 1 0
+        # Consequence: (memory xor states) -> new memory
+        self.memory = [v != s for s, v in zip(states, self.memory)]
+        yield
+
 
     @property
     def memory_size(self):
         return len(self.memory)
 
 
-    def react(self, states:iter) -> tuple:
-        """Return self response to given input neuron states.
-
-        states: iterable of booleans, giving the state of input neurons.
-        return: (tuple of directions, result of reaction to environment,
-                 new memory state)
-
-        """
-        # Atoms creation:
-        #  - define the neural network
-        #  - add an atom up/1 or down/1 foreach input neuron according to its state
-        input_atoms = self.neural_network + ''.join(
-            'up(' + str(idn) + ').'
-            # position in list gives the neuron id
-            for idn, is_up in enumerate(states)
-            if is_up
-        ) + ''.join(  # add the memory atoms
-            'up(' + str(idn) + ').'
-            for idn, is_up in enumerate(self.memory, start=self.nb_input_neuron)
-            if is_up
-        )
-        LOGGER.debug('INPUT ATOMS: "' + input_atoms + '"')
-        # ASP solver call
-        model = solving.model_from(input_atoms, FILE_ASP_RUNNING)
-        LOGGER.debug('OUTPUT ATOMS: ' + str(model))
-        # Memory rewritting: swap value if output neuron is up
-        written_memory = (int(atoms.arg(atom)) for atom in model
-                          if atom.startswith('memory('))
-        for mem_addr in written_memory:
-            self.memory[mem_addr] = not self.memory[mem_addr]
-        # Directions of movement extraction: get id of up-state output neurons
-        directions = tuple(Direction[atoms.arg(atom)] for atom in model
-                      if atom.startswith('direction('))
-        return tuple(Direction.simplified(directions))
+    @classmethod
+    def nb_neighbor_neuron(cls, nb_neighbor=default.NEIGHBOR_COUNT,
+                           nb_type=default.UNIT_TYPE_COUNT):
+        return nb_type * nb_neighbor
 
 
     def clone(self, mutator=None):
         """Return a copy of self, eventually mutated by given mutator"""
         # copy the data, mutate it if any mutator given
-        nb_intermediate_neuron, neuron_types = (self.nb_intermediate_neuron,
-                                                self.neuron_types)
+        nb_intermediate_neuron= self.nb_intermediate_neuron
+        neuron_types = self.neuron_types
         edges = self.edges
+
         if mutator:
             nb_intermediate_neuron, neuron_types, edges = mutator.mutate(
                 self.nb_intermediate_neuron, self.nb_neuron,
@@ -186,62 +120,7 @@ class NeuralNetwork:
             )
 
         return NeuralNetwork(
-            edges=edges,
+            edges=edges, neuron_types=neuron_types,
             memory_size=self.memory_size,
             nb_inter_neuron=nb_intermediate_neuron,
-            neuron_types=neuron_types,
-            nb_input_neuron=self.nb_input_neuron,
-            nb_output_neuron=self.nb_output_neuron,
         )
-
-
-    @staticmethod
-    def neuron_total_count(nb_inter, memory_size, nb_input, nb_output):
-        """Return the total number of neuron present in an individual"""
-        return sum((
-            nb_input,
-            nb_output,
-            memory_size * 2,  # input memory neurons + output memory neurons
-            nb_inter
-        ))
-
-
-    @staticmethod
-    def neuron_type_total_count(nb_inter, memory_size, nb_input, nb_output):
-        """Return the total number of needed neuron type"""
-        return sum((
-            nb_output,
-            memory_size,  # only the output memory neurons needs a type
-            nb_inter
-        ))
-
-    @staticmethod
-    def cleaned(network_atoms:str) ->str:
-        """Return a cleaned version of given neural network to remove
-        useless neuron, give an orientation to edges,..."""
-        model = solving.model_from(network_atoms, FILE_ASP_CLEANING)
-        assert model is not None, 'cleaning network lead to non existing model'
-        return '.'.join(model) + ('.' if len(model) else '')
-
-
-    @staticmethod
-    def from_string(atoms, nb_neuron:int=None, nb_input=None, nb_output=None,
-                    neuron_types=NeuronType.ixano):
-        """Return a new NeuralNetwork instance, initialized from given
-        string of atoms.
-        Note that all the field may be badly initialized."""
-        if nb_neuron is None:
-            nb_neuron = atoms.count('neuron')
-        if nb_output is None:
-            nb_output = atoms.count('output')
-        if nb_input is None:
-            nb_input = atoms.count(',' + NeuronType.INPUT.value + ')')
-        nb_inter = nb_neuron - nb_output - nb_input
-        instance = NeuralNetwork((), nb_inter, nb_input,
-                                 nb_output, neuron_types)
-        instance.neural_network = atoms
-        return instance
-
-
-    def __str__(self):
-        return self.neural_network
---
+++ neural_world/neural_network_engine.py
@@ -0,0 +1,208 @@
+"""Definition of the NeuralNetworkEngine base class."""
+
+import itertools
+
+from neural_world import commons
+from neural_world import default
+from neural_world import solving
+from neural_world.commons import (NeuronType, Direction,
+                                  FILE_ASP_CLEANING, FILE_ASP_RUNNING)
+
+
+LOGGER = commons.logger(commons.SUBLOGGER_LIFE)
+MINIMAL_NEURON_ID = 1
+
+
+class NeuralNetworkEngine:
+    """Base class for the NeuralNetwork class. Define the underlying behavior
+    of neural networks, as an abstract object.
+
+    Each neural network figures a set of atoms, and provides an API for
+    building new neural networks, reacting to neighbors states and cloning.
+
+    A neural network is a string, containing ASP-formatted data as atoms as:
+        - neuron(I,T): neuron of id I is of type T (T in IXANO)
+        - output(I): neuron of id I is an output neuron
+        - edge(I,J): there is an edge between neurons of id I and J
+
+
+    The object assume that inputs and outputs attributes are iterables
+    of pairs (f, n), where f is a function and n the number of neuron
+    associated to the function.
+    For input neurons, function takes no parameters, and should return
+    an iterable of n boolean values.
+    For output neurons, function receive an iterable of n boolean values,
+    and can yield Action.
+
+    Subclasses of NeuralNetworkEngine should define the content of inputs
+    and outputs, and expose them through the NeuralNetworkEngine.__init__ method.
+
+    """
+
+    def __init__(self, nb_intermediate_neuron:int, inputs:tuple, outputs:iter,
+                 edges:iter=None, neuron_types:iter=None):
+        self.inputs, self.outputs = tuple(inputs), tuple(outputs)
+        self.nb_intermediate_neuron = nb_intermediate_neuron
+        self._nb_input_neuron = sum(int(n) for _, n in self.inputs)
+        self._nb_output_neuron = sum(int(n) for _, n in self.outputs)
+        if edges is not None and neuron_types is not None:
+            self.build(edges, neuron_types)
+        else:
+            self.edges, self.neuron_types = None, None  # build() call necessary
+
+    def build(self, edges:iter, neuron_types:iter):
+        """Build the neural network using input data and attributes"""
+        self.edges, self.neuron_types = tuple(edges), tuple(neuron_types)
+
+        neuron_ids = iter(range(MINIMAL_NEURON_ID, self.nb_neuron + MINIMAL_NEURON_ID))
+        neuron_type = iter(self.neuron_types)
+        network_atoms = '.'.join(itertools.chain(
+            # input neurons
+            ('neuron(' + str(idn) + ','
+             + default.INPUT_NEURON_TYPE.value + ')'
+             for idn in itertools.islice(neuron_ids, 0, self.nb_input_neuron)),
+            # intermediate neurons
+            ('neuron(' + str(idn) + ',' + next(neuron_type).value + ')'
+             for idn in itertools.islice(neuron_ids, 0, self.nb_intermediate_neuron)),
+            # output neurons: give their type and their output status.
+            ('neuron(' + str(idn) + ',' + next(neuron_type).value + ').'
+             + 'output(' + str(idn) + ')'  # this neuron is an output
+             for idn in neuron_ids),
+            # edges
+            ('edge(' + str(id1) + ',' + str(id2) + ')'
+             for id1, id2 in self.edges)
+        )) + '.'
+        assert network_atoms.count('neuron') == self.nb_neuron
+        assert network_atoms.count('edge') == len(self.edges)
+        assert network_atoms.count('output') == self.nb_output_neuron
+        assert network_atoms.count(',i)') == self.nb_input_neuron
+        assert ('neuron(' + str(MINIMAL_NEURON_ID)) in network_atoms
+        assert ('neuron(' + str(self.maximal_neuron_id)) in network_atoms
+        assert ('neuron(' + str(MINIMAL_NEURON_ID-1)) not in network_atoms
+        # Cleaning, for remove useless data
+        self.neural_network_all = network_atoms
+        self.neural_network = NeuralNetworkEngine.cleaned(network_atoms)
+        LOGGER.debug('NEW NEURAL NETWORK: ' + self.neural_network_all)
+        LOGGER.debug('CLEANED: ' + self.neural_network)
+
+    def input_states(self, kwargs):
+        for input_func, nb_neuron in self.inputs:
+            states = input_func(kwargs)
+            assert len(states) == nb_neuron
+            yield from states
+
+    def react(self, **kwargs):
+        """Call all output functions, based on reactions of the neural network
+        to input functions, and yield their results."""
+        self.reaction_data = kwargs
+        output_states = self.output_from(self.input_states(kwargs))
+        assert self.nb_output_neuron == len(output_states)
+        output_states = iter(output_states)
+        for output_func, values in self.outputs:
+            yield from output_func(tuple(itertools.islice(output_states, 0, values)),
+                                   kwargs)
+
+
+    def output_from(self, input_states:iter) -> tuple:
+        """Return output states responding to given input neuron states.
+
+        states: iterable of booleans, giving the state of input neurons.
+        return: iterable of booleans, giving the state of output neurons.
+
+        """
+        # Atoms creation:
+        #  - define the neural network
+        #  - add an atom up/1 or down/1 foreach input neuron according to its state
+        input_atoms = self.neural_network + ''.join(
+            'up(' + str(idn) + ').'
+            # position in list gives the input neuron id
+            for idn, is_up in enumerate(input_states, start=MINIMAL_NEURON_ID)
+            if is_up
+        )
+        LOGGER.debug('INPUT ATOMS: "' + input_atoms + '"')
+        # ASP solver call
+        model = solving.model_from(input_atoms, FILE_ASP_RUNNING)
+        LOGGER.debug('OUTPUT ATOMS: ' + str(model))
+        up_outputs = set(int(atom.split('(')[1].strip(')'))
+                         for atom in model if atom.startswith('up'))
+        min_output_neuron_id = self.min_output_neuron_id
+        ret = tuple(idx in up_outputs for idx in range(min_output_neuron_id,
+                                                       self.maximal_neuron_id+1))
+        LOGGER.debug('OUTPUT STATES: ' + str(ret))
+        return ret
+
+    @property
+    def maximal_neuron_id(self):
+        return sum((
+            self.nb_input_neuron,
+            self.nb_intermediate_neuron,
+            self.nb_output_neuron,
+        ))
+
+    @property
+    def nb_input_neuron(self):
+        return self._nb_input_neuron
+
+    @property
+    def nb_output_neuron(self):
+        return self._nb_output_neuron
+
+    @property
+    def nb_neuron(self):
+        """Return the total number of neuron"""
+        return NeuralNetworkEngine.neuron_total_count(
+            self.nb_intermediate_neuron,
+            self.nb_input_neuron,
+            self.nb_output_neuron
+        )
+
+    @property
+    def min_output_neuron_id(self):
+        return self.nb_neuron - self.nb_output_neuron + MINIMAL_NEURON_ID
+
+    @staticmethod
+    def neuron_type_total_count(nb_intermediate, nb_output):
+        """Return the total number of needed neuron XANO type"""
+        return sum((nb_intermediate, nb_output))
+
+    @property
+    def nb_neuron_type(self):
+        return NeuralNetworkEngine.neuron_type_total_count(self.nb_intermediate_neuron,
+                                                           self.nb_output_neuron)
+
+    @staticmethod
+    def neuron_total_count(nb_inter, nb_input, nb_output):
+        """Return the total number of neuron present in an individual"""
+        return sum((nb_input, nb_inter, nb_output))
+
+
+    @staticmethod
+    def cleaned(network_atoms:str) ->str:
+        """Return a cleaned version of given neural network to remove
+        useless neuron, give an orientation to edges,..."""
+        model = solving.model_from(network_atoms, FILE_ASP_CLEANING)
+        assert model is not None, 'cleaning network lead to non existing model'
+        return '.'.join(model) + ('.' if len(model) else '')
+
+
+    @staticmethod
+    def from_string(atoms, nb_neuron:int=None, nb_input=None, nb_output=None,
+                    neuron_types=NeuronType.ixano):
+        """Return a new NeuralNetwork instance, initialized from given
+        string of atoms.
+        Note that all the field may be badly initialized."""
+        if nb_neuron is None:
+            nb_neuron = atoms.count('neuron')
+        if nb_output is None:
+            nb_output = atoms.count('output')
+        if nb_input is None:
+            nb_input = atoms.count(',' + NeuronType.INPUT.value + ')')
+        nb_inter = nb_neuron - nb_output - nb_input
+        instance = NeuralNetwork((), nb_inter, nb_input,
+                                 nb_output, neuron_types)
+        instance.neural_network = atoms
+        return instance
+
+
+    def __str__(self):
+        return self.neural_network
